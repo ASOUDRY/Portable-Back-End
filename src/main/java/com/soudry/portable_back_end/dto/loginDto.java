e6b4d9221055c20f03eda111e63c756d2285031d
Alexander Soudry <soudryalex@gmail.com>
2026-03-20 19:09:29 -0400

    added dto records for sending and receiving data from a user.

---
+++ src/main/java/com/soudry/portable_back_end/dto/loginDto.java
@@ -0,0 +1,5 @@
+package com.soudry.portable_back_end.dto;
+
+public record loginDto(String username, String password) {
+    
+}
---
+++ src/main/java/com/soudry/portable_back_end/dto/registerDto.java
@@ -0,0 +1,5 @@
+package com.soudry.portable_back_end.dto;
+
+public record registerDto(String username, String password, String email) {
+    
+}
