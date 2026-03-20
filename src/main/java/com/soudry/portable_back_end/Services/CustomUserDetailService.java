48d48b8606b52ae607894b6312d693345a82a935
Alexander Soudry <soudryalex@gmail.com>
2026-03-20 19:11:46 -0400

    Service for validating user with spring securit.

---
+++ src/main/java/com/soudry/portable_back_end/Services/CustomUserDetailService.java
@@ -0,0 +1,30 @@
+package com.soudry.portable_back_end.Services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.User;
+import com.soudry.portable_back_end.entities.Users;
+import com.soudry.portable_back_end.repo.UserRepo;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+   private final UserRepo userRepo;
+
+    public CustomUserDetailService(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Users user = userRepo.findByName(username)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        return User.builder()
+            .username(user.getName())
+            .password(user.getPassword())
+            .roles(user.getRole())
+            .build();
+    }
+}
