460d3efaaea05032bebb4fb2d64761fe0d8f9d08
Alexander Soudry <soudryalex@gmail.com>
2026-03-20 19:10:20 -0400

    started adding entities.

---
+++ src/main/java/com/soudry/portable_back_end/entities/Users.java
@@ -0,0 +1,47 @@
+package com.soudry.portable_back_end.entities;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import com.fasterxml.uuid.Generators;
+
+@Entity
+public class Users {
+
+    @Id
+    private String id;
+    private String name;
+    private String password;
+    private String email;
+    private String role;
+
+    // for reflection
+    public Users() { }
+
+    public Users(String name, String password, String role, String email) {
+        id = Generators.randomBasedGenerator().generate().toString();
+        this.name = name;
+        this.password = password;
+        this.role = role;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+}
